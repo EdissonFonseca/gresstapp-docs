e89a51547beee011412f8e46754d86abb018391b
Edisson Fonseca <edisson.fonseca@gmail.com>
2025-05-01 22:51:41 -0500

    fix: change broken links handling to warning

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -20,7 +20,7 @@ const config: Config = {
   organizationName: 'EdissonFonseca', // Usually your GitHub org/user name.
   projectName: 'gresstapp-docs', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
