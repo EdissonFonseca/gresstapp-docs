aa41016b9b16db4f96c7f6df80d25cf6b983335f
Edisson Fonseca <edisson.fonseca@gmail.com>
2025-05-01 22:48:35 -0500

    fix: update GitHub Pages configuration

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,14 +10,14 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://docs.gresst.com',
+  url: 'https://edissonfonseca.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/gresstapp-docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'gresst', // Usually your GitHub org/user name.
+  organizationName: 'EdissonFonseca', // Usually your GitHub org/user name.
   projectName: 'gresstapp-docs', // Usually your repo name.
 
   onBrokenLinks: 'throw',
