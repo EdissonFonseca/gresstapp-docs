85eb999abb6fa5f4d29ebb21ad801a82f2e2cd6b
Edisson Fonseca <edisson.fonseca@gmail.com>
2025-05-03 08:22:52 -0500

    Changes in url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,10 +10,10 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://edissonfonseca.github.io',
+  url: 'https://docsapp.gresst.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/gresstapp-docs/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
